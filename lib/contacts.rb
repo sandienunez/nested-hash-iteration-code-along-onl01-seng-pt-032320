c9ac84beb9bff191dd7c224770226a341f93c632
Sandie Nunez <snunez@vt.edu>
2020-04-05 22:08:29 +0000

    Automatically backed up by Learn

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -16,10 +16,13 @@ require 'pry'
   
 def remove_strawberry(contacts)
   contacts.each do |person, contact_details_hash|
+  icontacts.each do |person, contact_details_hash|
   if person == "Freddy Mercury"
     contact_details_hash.each do |attribute, data|
-      binding.pry
+      if attribute == :favorite_ice_cream_flavors
+        binding.pry
       end
     end
   end
+end
 end 
